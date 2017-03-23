f52c3b497e3c3fd080220a1bf4cded856a3f2504
Vu Dinh Hao <vu.dinh.hao@framgia.com>
2017-03-23 21:11:37 +0700

    Authenticate via Google

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,19 @@
 class User < ApplicationRecord
-	before_save { self.email = email.downcase }
-	validates :name,  presence: true, length: { maximum: 50 }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  	validates :email, presence: true, length: { maximum: 255 },
-  		format: { with: VALID_EMAIL_REGEX },
-  		uniqueness: { case_sensitive: false }
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
-  	has_secure_password
-  	validates :password, presence: true, length: { minimum: 6 }
-  	has_one :curriculum_vitae
+  def self.from_omniauth(access_token)
+  data = access_token.info
+  user = User.where(:email => data["email"]).first
 
+  unless user
+    password = Devise.friendly_token[0,20]
+    user = User.create(name: data["name"], email: data["email"],
+      password: password, password_confirmation: password
+    )
+  end
+  user
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # devise_for :users
+  devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
   root 'static_pages#home'
   # get 'curriculum_vitaes/new'
 
