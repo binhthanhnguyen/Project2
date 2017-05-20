cc2a6bdc42637c94f516b54b9d41982ef861e454
binhthanhnguyen <nguyen.thanh.binh@framgia.com>
2017-05-20 15:09:58 +0700

    Update uris for authentication

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,1 +1,1 @@
-OmniAuth.config.full_host = Rails.env.production? ? 'http://sie-internship-management.herokuapp.com/' : 'http://localhost:3000'
+OmniAuth.config.full_host = Rails.env.production? ? 'https://sie-internship-management.herokuapp.com/' : 'http://localhost:3000'
